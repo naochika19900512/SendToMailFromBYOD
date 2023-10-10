85b84c6b48fb1f56e8c078a1c6427d272a01fc0b
Naochika Yonezumi <125625688+naochika19900512@users.noreply.github.com>
2023-10-10 17:53:23 +0900

    init-git
    初回作成

---
+++ SendToMailFromBYOD.js
@@ -0,0 +1,25 @@
+function printJobHook(inputs, actions) {
+  // ジョブ分析が完了していない場合、関数を終了します
+  if (!inputs.job.isAnalysisComplete) {
+    return;
+  }
+  if (inputs.job.isWebPrintJob || inputs.job.isMobilityPrintJob){
+    // 受信者のメールアドレスを取得します
+    var emailRecipients = inputs.user.email;
+    
+    // メールの件名を設定します
+    var emailSubject = "新しいプリントジョブが送信されました";
+    
+    // メールの本文を作成します
+    var emailBody = "ユーザー " + inputs.job.username + " がプリントジョブを送信しました。ジョブの詳細:\n" +
+        "ドキュメント名: " + inputs.job.documentName + "\n" +
+        "残高:" +  inputs.user.balance + "\n" +
+        "ページ数: " + inputs.job.totalPages + "\n" +
+        "コピー数: " + inputs.job.copies + "\n" +
+        "コスト:" + inputs.job.cost;    
+    // メールを送信します
+    actions.utils.sendEmail(emailRecipients, emailSubject, emailBody);
+  }
+}
+
+​
